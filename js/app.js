36497759d5c513875b4f693cf5b2f174929306be
Chauhanrajk007 <Chauhanrajk007@users.noreply.github.com>
2026-09-20 01:49:15 +0530

    chore: rebrand project name to Traceback across UI, Razorpay, and LICENSE

--- js/app.js
+++ js/app.js
@@ -414,7 +414,7 @@ const App = (() => {
           key: keyId,
           amount,
           currency: "INR",
-          name: "Leave a Trace",
+          name: "Traceback",
           description: label,
           image: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🧭</text></svg>",
           prefill: { email: user.username },
